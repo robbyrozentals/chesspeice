6e53fc9ac1802ba31d753dfaf1a50b1236c9f86e
robert <robert@robert-ThinkPad-T450s>
2022-07-02 15:54:04 -0400

    I change the height from 40 to 30
    to make the rook smaller

--- rook.groovy
+++ rook.groovy
@@ -1,1 +1,19 @@
-//Your code here
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+
+//20 milamerters = diamiter, radius is going to be ten
+
+double diamiter =20
+double radius = diamiter/2
+double heightOfBase = radius/2
+double heightOfMiddle = 30
+CSG base = new Cylinder(radius,heightOfBase).toCSG()
+CSG middle = new Cylinder(radius-1, radius/2, heightOfMiddle).toCSG()
+CSG Top = new Cylinder( radius,heightOfBase).toCSG()
+			.movez(heightOfMiddle)
+
+
+
+
+
+return[base,middle,Top]
